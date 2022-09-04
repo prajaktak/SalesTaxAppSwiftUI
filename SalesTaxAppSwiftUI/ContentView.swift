d0e5805c133899b9e87a860ecd6331ad1169a5e0
Prajakta <prachee.j@gmail.com>
2022-09-04 10:55:22 +0200

    Initial Commit

---
+++ SalesTaxAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SalesTaxAppSwiftUI
+//
+//  Created by Prajakta Kulkarni on 04/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SalesTaxAppSwiftUI/SalesTaxAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesTaxAppSwiftUIApp.swift
+//  SalesTaxAppSwiftUI
+//
+//  Created by Prajakta Kulkarni on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SalesTaxAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
